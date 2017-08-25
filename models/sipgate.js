f86cd38276b4f6a674c9072940292b7f3dc7a331
Stefan Lange Hegermann <lange-hegermann@sipgate.de>
2017-08-25 11:41:15 +0200

    more help in display boxes

--- models/sipgate.js
+++ models/sipgate.js
@@ -2,6 +2,67 @@ const http = require("https"),
       querystring = require('querystring'),
       config = require('../config')
 
+exports.getClients = (bearer) => {
+    return get('/v2/authorization/oauth2/clients', bearer)
+}
+
+exports.getToken = (code, redirectUri) => {
+    const query = {
+        client_id: config.client_id,
+        client_secret: config.client_secret,
+        code: code,
+        grant_type: "authorization_code",
+        redirect_uri: redirectUri
+    };
+
+    return post('/v1/authorization/oauth/token', query)
+}
+
+exports.createClient = (name, description, redirectUrls, originUrls, bearer) => {
+    const query = {
+        "name": name,
+        "description": description,
+        "redirectUris": redirectUrls.split(","),
+        "webOrigins": originUrls.split(",")
+    }
+    return post('/v2/authorization/oauth2/clients', query, bearer);
+}
+
+exports.deleteClient = (clientid, bearer) => {
+    return del('/v2/authorization/oauth2/clients/'+clientid, bearer);
+}
+
+exports.getBalance = (bearer) => {
+    return get('/v2/balance', bearer)
+}
+
+exports.getUserInfo = (bearer) => {
+    return get('/v2/authorization/userinfo', bearer)
+}
+
+exports.getUserDetails = (userid, bearer) => {
+    return get('/v2/users/'+userid, bearer)
+}
+
+exports.getNumbers = (bearer) => {
+    return get('/v2/numbers', bearer)
+}
+
+exports.getWebhookUrls = (bearer) => {
+    return get('/v2/settings/sipgateio', bearer)
+}
+
+exports.setWebhookURLs = (incoming, outgoing, bearer) => {
+    const query = {
+        "incomingUrl": incoming,
+        "outgoingUrl": outgoing
+    }
+    return put('/v2/settings/sipgateio', query, bearer)
+}
+
+exports.getHistory = (bearer) => {
+    return get('/v2/w3/history?offset=0&limit=1000', bearer)
+}
 
 function request(method, url, querydata, bearer) {
     return new Promise((resolve, reject) => {
@@ -76,66 +137,4 @@ function get(url, bearer) {
 
 function put(url, querydata, bearer) {
     return request("PUT", url, querydata, bearer)
-}
-
-exports.getClients = (bearer) => {
-    return get('/v2/authorization/oauth2/clients', bearer)
-}
-
-exports.getToken = (code, redirectUri) => {
-    const query = {
-        client_id: config.client_id,
-        client_secret: config.client_secret,
-        code: code,
-        grant_type: "authorization_code",
-        redirect_uri: redirectUri
-    };
-
-    return post('/v1/authorization/oauth/token', query)
-}
-
-exports.createClient = (name, description, redirectUrls, originUrls, bearer) => {
-    const query = {
-        "name": name,
-        "description": description,
-        "redirectUris": redirectUrls.split(","),
-        "webOrigins": originUrls.split(",")
-    }
-    return post('/v2/authorization/oauth2/clients', query, bearer);
-}
-
-exports.deleteClient = (clientid, bearer) => {
-    return del('/v2/authorization/oauth2/clients/'+clientid, bearer);
-}
-
-exports.getBalance = (bearer) => {
-    return get('/v2/balance', bearer)
-}
-
-exports.getUserInfo = (bearer) => {
-    return get('/v2/authorization/userinfo', bearer)
-}
-
-exports.getUserDetails = (userid, bearer) => {
-    return get('/v2/users/'+userid, bearer)
-}
-
-exports.getNumbers = (bearer) => {
-    return get('/v2/numbers', bearer)
-}
-
-exports.getWebhookUrls = (bearer) => {
-    return get('/v2/settings/sipgateio', bearer)
-}
-
-exports.setWebhookURLs = (incoming, outgoing, bearer) => {
-    const query = {
-        "incomingUrl": incoming,
-        "outgoingUrl": outgoing
-    }
-    return put('/v2/settings/sipgateio', query, bearer)
-}
-
-exports.getHistory = (bearer) => {
-    return get('/v2/w3/history?offset=0&limit=1000', bearer)
 }
